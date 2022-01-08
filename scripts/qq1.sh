51edefae7109fe915aae285c99e0d3f43b2c4470
alok <alokwinwork@gmail.com>
2022-01-08 21:02:29 +0530

    initial commit1qsx

--- scripts/qq1.sh
+++ scripts/qq1.sh
@@ -1,12 +1,16 @@
-
-date
+gcloud compute instance-groups unmanaged remove-instances alok-testcicd-instance-group-1 --zone=us-central1-a --instances=nitin-nodejs-instance-1
 status=$?
 if test $status -eq 0
 then
-ls
+gcloud compute ssh alokwinwork@nitin-nodejs-instance-1 --zone=us-central1-a --command='sudo /bin/sh /home/alokwinwork/pull1.sh'
 status1=$?
-if test $status1 -eq 0
-then
-echo hello
+if test $status1 -ne 0
+gcloud compute instance-groups unmanaged add-instances alok-testcicd-instance-group-1 --zone=us-central1-a --instances=nitin-nodejs-instance-1
 fi
 fi
+
+
+
+
+
+
