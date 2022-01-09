75594714ad1ad9fdcf996dfa242af37221d73227
alok <alokwinwork@gmail.com>
2022-01-09 07:41:25 +0530

    initial commit1qsxbvd52sqqq1a1s

--- scripts/tempqq1.sh
+++ scripts/tempqq1.sh
@@ -1,4 +1,4 @@
-gcloud compute ssh alokwinwork@nitin-nodejs-instance-1 --zone=us-central1-a --command='sudo /bin/sh /home/alokwinwork/temp.sh'
+gcloud compute ssh alokwinwork@nitin-nodejs-instance-1 --zone=us-central1-a --command='sudo /bin/sh /home/alokwinwork/temp1.sh'
 
 
 
