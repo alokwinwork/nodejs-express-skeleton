6365aac782a061ea60c389afd8d1c2114578acea
alok <alokwinwork@gmail.com>
2022-01-13 15:32:19 +0530

    aaa

--- scripts/tempqq1.sh
+++ scripts/tempqq1.sh
@@ -1,6 +1,6 @@
 x=$1
 echo "First arg: $1" 
-gcloud compute ssh alokwinwork@nitin-nodejs-instance-1 --zone=us-central1-a --command='/bin/sh /home/alokwinwork/pull1.sh $x'
+gcloud compute ssh alokwinwork@nitin-nodejs-instance-1 --zone=us-central1-a --command='x=$1; /bin/sh /home/alokwinwork/pull1.sh $x'
 
 
 
