60d20c5859846b4e756b5d289ded7936530f58b3
alok <alokwinwork@gmail.com>
2022-01-13 18:59:53 +0530

    ssss

--- scripts/tempqq1.sh
+++ scripts/tempqq1.sh
@@ -1,5 +1,5 @@
 echo "First arg: $1" 
-gcloud compute ssh alokwinwork@nitin-nodejs-instance-1 --zone=us-central1-a --command="/bin/sh /home/alokwinwork/pull.sh $1"
+gcloud compute ssh alokwinwork@nitin-nodejs-instance-1 --zone=us-central1-a --command="sudo /bin/sh /home/alokwinwork/pull.sh $1"
 
 
 
